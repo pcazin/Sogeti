06eae8b936d62af73a4ad5ead6e5c75f7246c178
pcazin <pablocazin.dev@gmail.Com>
2023-02-24 18:21:00 +0100

    add new todo

--- sogeti-test-client/src/app/app.module.ts
+++ sogeti-test-client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import {MatInputModule } from '@angular/material/input';
 import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { NewTodoComponent } from './components/new-todo/new-todo.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { TodoDetailsComponent } from './components/todo-details/todo-details.com
     TodoListComponent,
     TodoComponent,
     TodoDetailsComponent,
+    NewTodoComponent
   ],
   imports: [
     BrowserModule,
